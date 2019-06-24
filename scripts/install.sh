6931c395ac354ca884d351cc371ce96fb5865380
Sreyas Ravichandran <sreyasravichandran@gmail.com>
2019-06-25 00:13:25 +0530

    Update install

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,10 +1,6 @@
 echo "Starting install..."
 TARGET_DIR="$HOME/api/server_app"
-<<<<<<< HEAD
-SOURCE_CODE="https://github.com/LiquidGalaxyLab/liquid-galaxy-api"
-=======
 SOURCE_CODE="https://github.com/Sreyas-108/server_app"
->>>>>>> 2e79be8a64b9f18ca2230cfac31496aa68110f9b
 sudo apt-get update
 sudo apt-get install python3.6
 sudo apt-get install python3-venv
