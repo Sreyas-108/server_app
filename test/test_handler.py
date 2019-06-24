df561e0b232b74245a194085d8af40f4b9c0123a
Sreyas Ravichandran <sreyasravichandran@gmail.com>
2019-06-24 15:44:12 +0530

    Tests various params for OSC message handler.

---
+++ test/test_handler.py
@@ -0,0 +1,42 @@
+import logging
+import unittest
+from unittest.mock import patch
+
+from app.controller.MessageHandler import handle
+from app.controller.ModuleType import ModuleType
+
+
+class HandlerTest(unittest.TestCase):
+    """Tests for handler of the OSC Server."""
+
+    @patch('app.controller.MessageHandler.ServerValidation')
+    @patch('app.controller.MessageHandler.OSCServer.OSCServer')
+    def test_exitModtype(self, mock_server, mock_validator):
+        basePath = '/testpath'
+        modtype = ModuleType.EXIT
+        mock_validator.return_value.validateID.return_value = True
+        mock_validator.return_value.basePath = basePath
+        mock_validator.getInstance.return_value = mock_validator.return_value
+
+        mock_server.getInstance.return_value = mock_server.return_value
+        mock_server.return_value.server_end.return_value = 0
+
+        handle(basePath + modtype.value[1], 0, modtype.value[0], 0)
+        mock_validator.return_value.validateID.assert_called_once()
+        mock_server.return_value.server_end.assert_called_once()
+
+    @patch('app.controller.MessageHandler.ServerValidation')
+    def test_invalidModType(self, mock_validator):
+        mock_validator.return_value.validateID.return_value = True
+        mock_validator.getInstance.return_value = mock_validator.return_value
+        with self.assertLogs(logging.getLogger(), logging.WARNING):
+            handle('', 0, 0, 0)
+        mock_validator.return_value.validateID.assert_called_once()
+
+    @patch('app.controller.MessageHandler.OSCSender')
+    def test_invalidParams(self, mock_sender):
+        with self.assertLogs(logging.getLogger(), logging.CRITICAL):
+            mock_sender.return_value.sendMessage.return_value = 0
+            mock_sender.getInstance.return_value = mock_sender.return_value
+            handle(0)
+            mock_sender.return_value.sendMessage.assert_called_once()
