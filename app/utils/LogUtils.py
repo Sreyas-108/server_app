0e89881b462a34f54800f089214389b220f8b110
Sreyas Ravichandran <sreyasravichandran@gmail.com>
2019-06-24 15:41:53 +0530

    Create LogUtils.py

---
+++ app/utils/LogUtils.py
@@ -0,0 +1,35 @@
+import logging
+import os
+from pathlib import Path
+
+
+class LogUtils:
+    """Utilities for Logging."""
+    path = Path(os.path.abspath(os.path.join(Path(os.path.abspath(os.pardir)).parent, 'logs')))
+    """Path of the logs file."""
+
+    @classmethod
+    def setUpLogs(cls):
+        """Create and set log file format."""
+        if not cls.path.exists():
+            os.mkdir(Path(os.path.abspath(os.path.join(Path(os.path.abspath(os.pardir)).parent, 'logs'))))
+        logging.basicConfig(filename=str(cls.path) + '/test.log', filemode='a', level=logging.DEBUG,
+                            format='%(asctime)s : %(name)s - %(levelname)s - %(message)s : [in %(pathname)s:%(lineno)d]')
+
+    @staticmethod
+    def writeWarning(data):
+        """Write warning log of [data]. For any failure."""
+        LogUtils.setUpLogs()
+        logging.warning(str(data))
+
+    @staticmethod
+    def writeCritical(data):
+        """Write critical log of [data]. For high priority error."""
+        LogUtils.setUpLogs()
+        logging.critical(str(data))
+
+    @staticmethod
+    def writeInfo(data):
+        """Write info log of [data]. For step debugging."""
+        LogUtils.setUpLogs()
+        logging.info(str(data))
