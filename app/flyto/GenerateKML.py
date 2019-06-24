fdb214bcda1ffdf25240815cc47ea3e1120fd5a6
Sreyas Ravichandran <sreyasravichandran@gmail.com>
2019-06-24 15:41:30 +0530

    Create KML files for the respective data.

---
+++ app/flyto/GenerateKML.py
@@ -0,0 +1,28 @@
+import os
+
+import simplekml
+
+from app.controller.ModuleType import ModuleType
+from app.controller.OSCSender import OSCSender
+from app.flyto.FlyToOptions import FlyToOptions
+from app.utils.KMLUtils import KMLUtils
+from app.utils.LogUtils import LogUtils
+
+
+def generateFlyTo(data):
+    """Generate KML file for fly to [data]."""
+    LogUtils.writeInfo("KML generation for Fly To.")
+    try:
+        flyto = FlyToOptions(data.lower())
+    except Exception as e:
+        LogUtils.writeWarning("Wrong target location for Fly To received : " + str(e))
+        OSCSender.getInstance().sendMessage(ModuleType.FLYTO, "Wrong target location for Fly To received.")
+        return
+    try:
+        kml = simplekml.Kml()
+        kml.hint = 'target=' + flyto.value
+        kml.save(KMLUtils.getFilePath())
+        os.startfile(KMLUtils.getFilePath())
+    except Exception as e:
+        LogUtils.writeWarning("KML generation for fly to failure : " + str(e))
+        OSCSender.getInstance().sendMessage(ModuleType.GESTURE, "KML generation for fly to failure.")
---
+++ app/gestures/GenerateKML.py
@@ -0,0 +1,27 @@
+import os
+
+import simplekml
+
+from app.controller.ModuleType import ModuleType
+from app.controller.OSCSender import OSCSender
+from app.utils.KMLUtils import KMLUtils
+from app.utils.LogUtils import LogUtils
+
+
+def generateGesture(data):
+    """Generate KML file for Gesture [data]."""
+    LogUtils.writeInfo("KML generation for Gesture.")
+    try:
+        kml = simplekml.Kml()
+        pnt = kml.newpoint()
+        pnt.lookat.gxaltitudemode = simplekml.GxAltitudeMode.relativetoseafloor
+        pnt.lookat.latitude = data.latitude
+        pnt.lookat.longitude = data.longitude
+        pnt.lookat.range = KMLUtils.convertZoomToRange(data.zoom)
+        pnt.lookat.heading = data.bearing
+        pnt.lookat.tilt = data.tilt
+        kml.save(KMLUtils.getFilePath())
+        os.startfile(KMLUtils.getFilePath())
+    except Exception as e:
+        LogUtils.writeWarning("KML generation for gesture failure : " + str(e))
+        OSCSender.getInstance().sendMessage(ModuleType.GESTURE, "KML generation for gesture failure.")
