f114668f4a1da3e745e5d4ccba987c304765372f
Sreyas Ravichandran <sreyasravichandran@gmail.com>
2019-06-24 15:42:46 +0530

    Tests for generating KML

---
+++ test/test_generateKML.py
@@ -0,0 +1,29 @@
+import logging
+import unittest
+from pathlib import Path
+
+from app.flyto.FlyToOptions import FlyToOptions
+from app.flyto.GenerateKML import generateFlyTo
+from app.gestures.GenerateKML import generateGesture
+from app.models.KMLData import KMLData
+from app.poi.GenerateKML import generatePOI
+from app.utils.KMLUtils import KMLUtils
+
+
+class GenerateKMLTest(unittest.TestCase):
+    """Tests for generation of KML for all module types."""
+
+    def test_POI(self):
+        with self.assertLogs(logging.getLogger(), logging.INFO):
+            generatePOI(KMLData())
+            self.assertEqual(Path(KMLUtils.getFilePath()).exists(), True)
+
+    def test_gestures(self):
+        with self.assertLogs(logging.getLogger(), logging.INFO):
+            generateGesture(KMLData())
+            self.assertEqual(Path(KMLUtils.getFilePath()).exists(), True)
+
+    def test_flyto(self):
+        with self.assertLogs(logging.getLogger(), logging.INFO):
+            generateFlyTo(FlyToOptions.MARS.value)
+            self.assertEqual(Path(KMLUtils.getFilePath()).exists(), True)
