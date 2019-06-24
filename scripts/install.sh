5013491a466697259f7627d0e09151190f6d07b5
Sreyas Ravichandran <sreyasravichandran@gmail.com>
2019-06-24 15:39:08 +0530

    Update install.sh

---
+++ scripts/install.sh
@@ -0,0 +1,14 @@
+echo "Starting install..."
+TARGET_DIR="$HOME/api"
+SOURCE_CODE="https://github.com/LiquidGalaxyLab/liquid-galaxy-api"
+sudo apt-get update
+sudo apt-get install python3.6
+sudo apt-get install python3-venv
+sudo apt-get install git-core
+git clone $SOURCE_CODE $TARGET_DIR
+cd "$TARGET_DIR"
+python3 -m venv venv
+virtualenv venv
+source venv/bin/activate
+pip install -r requirements.txt
+python ./app/controller/server.py
