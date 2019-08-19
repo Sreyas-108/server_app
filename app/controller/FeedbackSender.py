f2656ec7dbe857f1d51eed847deaba9d32e574f5
Sreyas Ravichandran <sreyasravichandran@gmail.com>
2019-08-19 22:50:29 +0530

    Add controller app ip

--- app/controller/FeedbackSender.py
+++ app/controller/FeedbackSender.py
@@ -1,10 +1,12 @@
 from app.controller.OSCSender import OSCSender
+import ipaddress
 
 
 class FeedbackSender:
     """Feedback functionalities."""
     __instance = None
     __sender = None
+    __ipAddress = ""
 
     @staticmethod
     def getInstance():
@@ -16,7 +18,19 @@ class FeedbackSender:
     def __init__(self):
         FeedbackSender.__instance = self
         self.__sender = OSCSender.getInstance()
+        __ipAddress = self._getIP()
 
     def sendMessage(self, mod_type, data):
         """Send feedback as an OSC message with [data]."""
-        self.__sender.sendMessage(mod_type, str(data), 100)
+        print(data)
+        self.__sender.sendMessage(mod_type, str(data), 1, self.__ipAddress)
+
+    def _getIP(self):
+        """Get IP address of controller application."""
+        print("Enter IP of controller app : ")
+        add = input()
+        try:
+            ipaddress.ip_address(add)
+            return add
+        except:
+            return "127.0.0.1"
--- app/controller/server.py
+++ app/controller/server.py
@@ -3,6 +3,7 @@ from threading import Thread
 from app.controller.OSCServer import OSCServer
 from app.controller.SlaveHandler import SlaveHandler
 from app.controller.Validation import ServerValidation
+from app.controller.FeedbackSender import FeedbackSender
 from app.utils.LogUtils import LogUtils
 
 
@@ -11,6 +12,7 @@ def startApplication():
     LogUtils.writeInfo("Starting application...")
     SlaveHandler.getInstance()
     ServerValidation.getInstance()
+    FeedbackSender.getInstance()
     server = OSCServer()
     sc_thread = Thread(target=server.server_start())
     sc_thread.start()
